Tupperbox/Sortable

commit 4aff1d4ac601f5c8c7c81c8758fd4ac2891014b1

Use Element.matches for improved selector support

Sortable.js
@@ -1427,15 +1427,11 @@
 
 	function _matches(/**HTMLElement*/el, /**String*/selector) {
 		if (el) {
-			selector = selector.split('.');
-
-			var tag = selector.shift().toUpperCase(),
-				re = new RegExp('\\s(' + selector.join('|') + ')(?=\\s)', 'g');
-
-			return (
-				(tag === '' || el.nodeName.toUpperCase() == tag) &&
-				(!selector.length || ((' ' + el.className + ' ').match(re) || []).length == selector.length)
-			);
+			if (el.matches) {
+				return el.matches(selector);
+			} else if (el.msMatchesSelector) {
+				return el.msMatchesSelector(selector);
+			}
 		}
 
 		return false;

commit ff4851a8c58e1907c369823dbe029169d587b082

Fix HTMLElement.match error introduced in 4aff1d4

Sortable.js
@@ -1432,9 +1432,17 @@
 	function _matches(/**HTMLElement*/el, /**String*/selector) {
 		if (el) {
 			if (el.matches) {
-				return el.matches(selector);
+				try {
+					return el.matches(selector);
+				} catch(e) {
+					return false;
+				}
 			} else if (el.msMatchesSelector) {
-				return el.msMatchesSelector(selector);
+				try {
+					return el.msMatchesSelector(selector);
+				} catch(e) {
+					return false;
+				}
 			}
 		}
 